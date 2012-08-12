1307852396b2d520f7e083f14dda0df3e0969c38
cuongnm92 <nmc2147483647@gmail.com>
2012-08-12 21:25:19 +0700

    fix BaseExecutor error

--- encryption-extension-repo/src/java/com/extendedencryption/action/executer/EncryptionActionExecuter.java
+++ encryption-extension-repo/src/java/com/extendedencryption/action/executer/EncryptionActionExecuter.java
@@ -1,28 +1,29 @@
 package com.extendedencryption.action.executer;
 
 import java.util.List;
+import com.extendedencryption.util.AES;
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ParameterDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 
-public class EncryptionActionExecuter extends ActionExecuterAbstractBase {
+public class EncryptionActionExecuter extends BaseExecuter {
 	
 	public static final String NAME = "encryption-action";
 	
     private NodeService nodeService;
 	
-    public void setNodeService(NodeService nodeService) {
-		this.nodeService = nodeService;
-	}
-    
 	protected void addParameterDefinitions(List<ParameterDefinition> paramList) {
     }
-		
+	
+	@Override
+	public void action(NodeRef nodeRefer) {
+		byte[] data = AES.encrypt(super.getNodeContent(nodeRefer), null);
+		super.write(nodeRefer, data);
+	}
+	
 	public void executeImpl(Action ruleAction, NodeRef actionedUponNodeRef) {
-		EncryptionExecuter encrypt = new EncryptionExecuter();
-		encrypt.setNodeService(this.nodeService);
-		encrypt.action(actionedUponNodeRef);
+		this.action(actionedUponNodeRef);
     } // end if isEmpty
 }
