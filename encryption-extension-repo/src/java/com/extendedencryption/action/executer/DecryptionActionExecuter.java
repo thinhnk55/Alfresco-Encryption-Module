4a408b2a80568de2e8e7f81dc060ff43e918da42
cuongnm92 <nmc2147483647@gmail.com>
2012-08-12 22:34:35 +0700

    fix bug

--- encryption-extension-repo/src/java/com/extendedencryption/action/executer/DecryptionActionExecuter.java
+++ encryption-extension-repo/src/java/com/extendedencryption/action/executer/DecryptionActionExecuter.java
@@ -1,28 +1,35 @@
 package com.extendedencryption.action.executer;
 
+/*
+ * Project: Alfresco Encryption Extension Module , part of the Creative Summer
+ * This code was developped by a group of 3 students from UET-VNU .
+ * License   : GNU General Public License, version 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
 import java.util.List;
+import com.extendedencryption.util.AES;
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ParameterDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 
-public class DecryptionActionExecuter extends ActionExecuterAbstractBase {
+public class DecryptionActionExecuter extends BaseExecuter {
 	
 	public static final String NAME = "decryption-action";
 	
     private NodeService nodeService;
-	
-    public void setNodeService(NodeService nodeService) {
-		this.nodeService = nodeService;
-	}
     
 	protected void addParameterDefinitions(List<ParameterDefinition> paramList) {
     }
-		
+	
+	@Override
+	public void action(NodeRef nodeRefer) {
+		byte[] data = AES.decrypt(super.getNodeContent(nodeRefer), null);
+		super.write(nodeRefer, data);
+	}
+	
 	public void executeImpl(Action ruleAction, NodeRef actionedUponNodeRef) {
-		DecryptionExecuter decrypt = new DecryptionExecuter();
-		decrypt.setNodeService(this.nodeService);
-		decrypt.action(actionedUponNodeRef);
+		this.action(actionedUponNodeRef);
     } // end if isEmpty
 }
